d93791331fecb2bfcfd28ec1c820c76bf460e55b
runAnwesh <anweshkm1122@gmail.com>
2024-08-07 00:03:23 +0530

    commits

--- src/test.py
+++ src/test.py
@@ -4,15 +4,15 @@ import requests
 url = "https://water-test-mrgh.onrender.com/predict"
 
 x_new = dict(   
-  ph: 120.0,
-  Hardness: 10.2,
-  Solids : 0.4,
-  Chloramines: 0.5,
-  Sulfate : 0.7,
-  Conductivity : 30.12,
-  Organic_carbon : 2.7,
-  Trihalomethanes : 61.7,
-  Turbidity : 4.5
+  ph = 120.0,
+  Hardness = 10.2,
+  Solids = 0.4,
+  Chloramines = 0.5,
+  Sulfate = 0.7,
+  Conductivity = 30.12,
+  Organic_carbon = 2.7,
+  Trihalomethanes = 61.7,
+  Turbidity = 4.5
 
 )
 

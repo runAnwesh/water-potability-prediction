10e1986ce77e460475347f21377c07b0a3aa5eac
runAnwesh <anweshkm1122@gmail.com>
2024-08-07 23:55:14 +0530

    Hyperparameter Tuning added

--- src/model_building.py
+++ src/model_building.py
@@ -2,14 +2,17 @@ import pandas as pd
 import numpy as np
 import os
 import pickle
+import yaml
 from sklearn.ensemble import RandomForestClassifier
 
 train_data = pd.read_csv("./data/processed/train_processed.csv")
 
+n_estimators = yaml.safe_load(open("params.yaml"))["model_building"]["n_estimators"]
+
 X_train = train_data.drop(columns=['Potability'],axis=1)
 Y_train = train_data['Potability']
 
-clf = RandomForestClassifier()
+clf = RandomForestClassifier(n_estimators=n_estimators)
 clf.fit(X_train,Y_train)
 
 pickle.dump(clf,open("model.pkl","wb"))
